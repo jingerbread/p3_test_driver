84d163b64bad836df9fc23c1721faa0d97001007
malygm1 <Maria.Malygina@emc.com>
2020-04-15 14:53:40 +0300

    [added] perf-pravega-tests/pravega-gentest_100b_1p_5e4_rate_2min.py

--- scripts/setup_p3_test_driver_on_jumphost.sh
+++ scripts/setup_p3_test_driver_on_jumphost.sh
@@ -108,3 +108,14 @@ ls dist/
 #  Uninstall previous and install new package from dist
 cd .. && yes | pip -v uninstall p3_test_driver
 pip  install p3_test_driver/dist/p3_test_driver-2.0.3-py3-none-any.whl
+
+echo -e "\e[36mSetup finished successfully at $(date)\e[0m"
+echo "\e[36mRun test with f.e:\e[0m"
+echo -e "\e[35m----Pravega tests:----\e[0m"
+echo -e "\e[36mtests/testgen_pravega_ssh.py -vv | p3_test_driver -t - -c config/pravega_ssh.config.yaml\e[0m"
+echo -e "\e[36mtests/perf-pravega-tests/pravega-gentest_100b_1p_5e4_rate_2min.py -vv | p3_test_driver -t - -c config/pravega_ssh.config.yaml\e[0m"
+
+echo -e "\e[35m----Pulsar tests:----\e[0m"
+echo -e "\e[36mtests/testgen_pulsar_ssh.py -vv | p3_test_driver -t - -c config/pulsar_ssh.config.yaml\e[0m" # Takes ~ >= 90 min
+echo -e "\e[36mtests/perf-pulsar-tests/pulsar-gentest_100b_1p_5e4_rate_2min.py -vv | p3_test_driver -t - -c config/pulsar_ssh.config.yaml\e[0m"
+echo -e "\e[36mtests/perf-pulsar-tests/pulsar-gentest_100b_1p_5e4_rate_2min.py -vv | p3_test_driver -t - -c config/pulsar_ssh.config.yaml\e[0m"
