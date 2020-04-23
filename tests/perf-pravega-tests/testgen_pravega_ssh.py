f3e51c284ddc8374d0532f581fceb37774995004
malygm1 <Maria.Malygina@emc.com>
2020-04-23 20:46:14 +0300

    [latest_master] 10k 1p rate: [1e5, 2e5, 3e5]

--- tests/perf-pravega-tests/testgen_pravega_ssh.py
+++ tests/perf-pravega-tests/testgen_pravega_ssh.py
@@ -53,38 +53,38 @@ tarball = '../package/target/openmessaging-benchmark-0.0.1-SNAPSHOT-bin.tar.gz'
 build = False
 
 # Message size 100 B
-for repeat in range(1):
-    for producerWorkers in [1]:
-        numWorkers = 0 if localWorker else producerWorkers*2
-        for testDurationMinutes in [1]:
-            for messageSize in [100]:
-                for producerRateEventsPerSec in [1e5, 2e5, 4e5, -1]:
-                    for topics in [1]:
-                        for partitionsPerTopic in [16]: #[1, 16]:
-                            for producersPerWorker in [1]:
-                                producersPerTopic = 1   # int(producersPerWorker * producerWorkers)
-                                for consumerBacklogSizeGB in [0]:
-                                    for subscriptionsPerTopic in [1]:
-                                        for consumerPerSubscription in [partitionsPerTopic]:
-                                            for includeTimestampInEvent in [True]:
-                                                add_test()
-
-# Message size 10 KB
 # for repeat in range(1):
 #     for producerWorkers in [1]:
 #         numWorkers = 0 if localWorker else producerWorkers*2
 #         for testDurationMinutes in [1]:
-#             for messageSize in [10000]:
-#                 for producerRateEventsPerSec in [3e5]: #[2e5, 3e5]: #-1]:
+#             for messageSize in [100]:
+#                 for producerRateEventsPerSec in [1e5, 2e5, 4e5, -1]:
 #                     for topics in [1]:
-#                         for partitionsPerTopic in [1]: #[1, 16]:
-#                             for producersPerWorker in [2]:
+#                         for partitionsPerTopic in [16]: #[1, 16]:
+#                             for producersPerWorker in [1]:
 #                                 producersPerTopic = 1   # int(producersPerWorker * producerWorkers)
 #                                 for consumerBacklogSizeGB in [0]:
 #                                     for subscriptionsPerTopic in [1]:
 #                                         for consumerPerSubscription in [partitionsPerTopic]:
 #                                             for includeTimestampInEvent in [True]:
 #                                                 add_test()
+
+# Message size 10 KB
+for repeat in range(1):
+    for producerWorkers in [1]:
+        numWorkers = 0 if localWorker else producerWorkers*2
+        for testDurationMinutes in [1]:
+            for messageSize in [10000]:
+                for producerRateEventsPerSec in [1e5, 2e5, 3e5]: #[2e5, 3e5]: #-1]:
+                    for topics in [1]:
+                        for partitionsPerTopic in [1]: #[1, 16]:
+                            for producersPerWorker in [2]:
+                                producersPerTopic = 1   # int(producersPerWorker * producerWorkers)
+                                for consumerBacklogSizeGB in [0]:
+                                    for subscriptionsPerTopic in [1]:
+                                        for consumerPerSubscription in [partitionsPerTopic]:
+                                            for includeTimestampInEvent in [True]:
+                                                add_test()
 
 
 print(json.dumps(test_list, sort_keys=True, indent=4, ensure_ascii=False))
