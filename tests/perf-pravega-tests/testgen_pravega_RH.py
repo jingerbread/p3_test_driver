5523298179637018fc9ba7a891e131ec1699497d
Malygina <Maria.Malygina@emc.com>
2020-06-17 16:54:19 +0300

    [changed] pravega 44 tests

--- tests/perf-pravega-tests/testgen_pravega_RH.py
+++ tests/perf-pravega-tests/testgen_pravega_RH.py
@@ -63,7 +63,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [100]:
-                for producerRateEventsPerSec in [-1]:   # [1e2, 1e3, 5e3, 1e4, 5e4, 6e4, 6e5, 1e6, -1]:
+                for producerRateEventsPerSec in [1e2, 1e3, 5e3, 1e4, 5e4, 6e4, 6e5, 1e6, -1]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -75,11 +75,11 @@ for repeat in range(1):
 
 # Message size 100 B 16 partitionsPerTopic 9 tests
 for repeat in range(1):
-    for producerWorkers in [4]:
+    for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [100]:
-                for producerRateEventsPerSec in [-1]:   # [1e2, 1e3, 5e3, 1e4, 5e4, 6e4, 6e5, 1e6, -1]:
+                for producerRateEventsPerSec in [1e2, 1e3, 5e3, 1e4, 5e4, 6e4, 6e5, 1e6, -1]:
                     for topics in [4]:
                         for partitionsPerTopic in [16]:
                             for producersPerWorker in [2]:
@@ -92,11 +92,11 @@ for repeat in range(1):
 
 # Message size 10k 1 partitionsPerTopic 13 tests
 for repeat in range(1):
-    for producerWorkers in [1]:
+    for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [15e3, 25e3, 35e3, 2e4, 3e4, 4e4, 5e4, 55e3, 6e4, -1]:  # 1e3, 3e3, 9e3,
+                for producerRateEventsPerSec in [1e3, 3e3, 9e3, 15e3, 25e3, 35e3, 2e4, 3e4, 4e4, 5e4, 55e3, 6e4, -1]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -108,7 +108,7 @@ for repeat in range(1):
 
 # Message size 10k 16 partitionsPerTopic 13 tests
 for repeat in range(1):
-    for producerWorkers in [1]:
+    for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
