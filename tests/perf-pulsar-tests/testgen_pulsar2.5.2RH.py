339de4cf624881fe0aef831149ef123487976fe0
Malygina <Maria.Malygina@emc.com>
2020-06-23 18:47:32 +0300

    [changed] rate for 10k [7e4, 1e3, 6e4, 1e4, 5e4, 6e3, 3e3, 5e3, 55e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]

--- tests/perf-pulsar-tests/testgen_pulsar2.5.2RH.py
+++ tests/perf-pulsar-tests/testgen_pulsar2.5.2RH.py
@@ -73,14 +73,13 @@ image = 'jingerbread/pulsar-omb:dev2.5.2-560d085'
 tarball = '../package/target/openmessaging-benchmark-0.0.1-SNAPSHOT-bin.tar.gz'
 build = False
 
-'''
 # Message size 10k 16 partitionsPerTopic 14 tests
 for repeat in range(1):
     for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [1e6, 1e3, 5e5, 6e4, 3e3, 5e4, 55e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [7e4, 1e3, 6e4, 1e4, 5e4, 6e3, 3e3, 5e3, 55e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [16]:
                             for producersPerWorker in [2]:
@@ -96,7 +95,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [1e6, 1e3, 5e5, 6e4, 3e3, 5e4, 55e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [7e4, 1e3, 6e4, 1e4, 5e4, 6e3, 3e3, 5e3, 55e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -140,7 +139,7 @@ for repeat in range(1):
                                         for consumerPerSubscription in [producersPerTopic]:
                                             add_test()
 
-
+'''
 
 print(json.dumps(test_list, sort_keys=True, indent=4, ensure_ascii=False))
 print('Number of tests generated: %d' % len(test_list), file=sys.stderr)
