52e02b6ebf26234e92b4e69b18c449c29868d4f7
Malygina <Maria.Malygina@emc.com>
2020-06-29 22:35:52 +0300

    [commented] 100b tests

--- tests/perf-pravega-tests/testgen_pravega_RH.py
+++ tests/perf-pravega-tests/testgen_pravega_RH.py
@@ -56,14 +56,14 @@ image = 'devops-repo.isus.emc.com:8116/maria/omb:dev-15b0f5eb9-29.06.2020'
 #image = 'devops-repo.isus.emc.com:8116/maria/omb:dev2.5.2-6f923b9-nonetty-1'
 tarball = '../package/target/openmessaging-benchmark-0.0.1-SNAPSHOT-bin.tar.gz'
 build = False
-'''
+
 # Message size 10k 16 partitionsPerTopic 16 tests
 for repeat in range(1):
     for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [5e4, 1e3, 1e4, 5e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [48e4, 1e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [16]:
                             for producersPerWorker in [2]:
@@ -79,7 +79,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [5e4, 1e3, 1e4, 5e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [48e3, 1e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -121,7 +121,7 @@ for repeat in range(1):
                                         for consumerPerSubscription in [producersPerTopic]:
                                                 add_test()
 
-
+'''
 
 print(json.dumps(test_list, sort_keys=True, indent=4, ensure_ascii=False))
 print('Number of tests generated: %d' % len(test_list), file=sys.stderr)
