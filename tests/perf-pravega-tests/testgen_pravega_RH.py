1424972bbebf8be2c31f3691c92fba2482cd635e
Malygina <Maria.Malygina@emc.com>
2020-07-05 19:55:22 +0300

    [changed] rates for pravega test, image devops-repo.isus.emc.com:8116/maria/omb:dev2.6.0-1c40f1e

--- tests/perf-pravega-tests/testgen_pravega_RH.py
+++ tests/perf-pravega-tests/testgen_pravega_RH.py
@@ -52,8 +52,7 @@ test_list = []
 localWorker = False
 namespace = 'default'
 ombHelmPath = '../deployment/kubernetes/helm/pulsar-benchmark'
-image = 'devops-repo.isus.emc.com:8116/maria/omb:dev-15b0f5eb9-29.06.2020'
-#image = 'devops-repo.isus.emc.com:8116/maria/omb:dev2.5.2-6f923b9-nonetty-1'
+image = 'devops-repo.isus.emc.com:8116/maria/omb:dev2.6.0-1c40f1e'
 tarball = '../package/target/openmessaging-benchmark-0.0.1-SNAPSHOT-bin.tar.gz'
 build = False
 
@@ -63,7 +62,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [48e4, 1e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [7e4, 5e2, 55e3, 7e3, 60e3, 1e3, 50e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [16]:
                             for producersPerWorker in [2]:
@@ -79,7 +78,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [10000]:
-                for producerRateEventsPerSec in [48e3, 1e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
+                for producerRateEventsPerSec in [7e4, 5e2, 55e3, 7e3, 60e3, 1e3, 50e3, 1e4, 6e3, 3e3, 5e3, 45e3, 9e3, 4e4, 15e3, 3e4, 25e3, 35e3, 2e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -88,14 +87,14 @@ for repeat in range(1):
                                     for subscriptionsPerTopic in [1]:
                                         for consumerPerSubscription in [producersPerTopic]:
                                             add_test()
-'''
+
 # Message size 100 B 16 partitionsPerTopic 9 tests
 for repeat in range(1):
     for producerWorkers in [2]:
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [100]:
-                for producerRateEventsPerSec in [1e6, 1e2, 5e5, 1e3, 6e5, 1e4, 6e4, 5e3, 5e4]:
+                for producerRateEventsPerSec in [-1, 1e6, 1e5, 9e5, 1e2, 5e5, 1e3, 6e5, 1e4, 6e4, 5e3, 5e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [16]:
                             for producersPerWorker in [2]:
@@ -111,7 +110,7 @@ for repeat in range(1):
         numWorkers = 0 if localWorker else producerWorkers*2
         for testDurationMinutes in [2]:
             for messageSize in [100]:
-                for producerRateEventsPerSec in [1e6, 1e2, 5e5, 1e3, 6e5, 1e4, 6e4, 5e3, 5e4]:
+                for producerRateEventsPerSec in [-1, 1e6, 1e5, 9e5, 1e2, 5e5, 1e3, 6e5, 1e4, 6e4, 5e3, 5e4]:
                     for topics in [4]:
                         for partitionsPerTopic in [1]:
                             for producersPerWorker in [2]:
@@ -121,7 +120,7 @@ for repeat in range(1):
                                         for consumerPerSubscription in [producersPerTopic]:
                                                 add_test()
 
-'''
+
 
 print(json.dumps(test_list, sort_keys=True, indent=4, ensure_ascii=False))
 print('Number of tests generated: %d' % len(test_list), file=sys.stderr)
